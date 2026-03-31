83fcabb4945b0aa576c5bced263a73ee44842e5c
evraday <77870155+evraday@users.noreply.github.com>
2026-03-31 01:41:02 +0100

    Update files - next.config.js

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
